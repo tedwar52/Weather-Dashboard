d720a1be96ba4a76ec0b0d71dfe7c2df4590390b
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-12-06 16:50:57 -0500

    api call is working

--- forecast.js
+++ forecast.js
@@ -1,11 +1,45 @@
+/*
+$(document).ready(function () {
+    var input = "";
+
+    $("#run-search").click(function () {
+        $("#search-term").each(function () {
+            $
+        })
+    })
+});
+*/
+/** 
+@returns {string}
+*/
+
 function buildQueryURL() {
 
     /*base url*/
     const URL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apikey;
+    
     /* should be: url + city name + api key */
     
     const city = "Asheville";
     const apikey = "&appid=a7006c74c28e21c72439b32d4b4920ec";
     
+    const citySearch = $("#search-term").val().trim();
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apikey;
+
+    console.log(queryURL);
 }
+/**
+ * @param {object} 
+ */
+
+$("#run-search").on("click", function(event) {
+    event.preventDefault();
+    /*clear();*/
+    var queryURL = buildQueryURL();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    });
+    console.log("It searched!");
+})
