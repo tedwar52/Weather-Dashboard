ed321a5bc56c1953eedb4b77ee4c617985a128e4
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-12-07 13:36:35 -0500

    5 day forecast is fully functional. Needs styling

--- 5day.js
+++ 5day.js
@@ -18,54 +18,45 @@ function buildLongURL() {
         url: longURL,
         method: "GET"
     }).then(function(response) {
-
-        console.log(response.list[0].dt_text);
-        //DAY 1
+        
         for (var i = 0; i < 5; i++) {
-            //date
-            console.log(response.list[i].main.temp); //THIS WORKS
+            //Create new card component for each day
+            var day = $("<div>");
+            day.addClass("card");
 
-            var date1 = $("<p>");
-            date1.text(response.list[i].dt_txt);
-            $("#forecast").append(date1);
+            //Date
+            var date = $("<p>");
+            date.text(response.list[i].dt_txt);
+            day.append(date);
+            $("#forecast").append(day);
+
+            //Weather Conditions Icon
+            var iconURL = response.list[i].weather[0].icon;
+            var icon = $("<img>").attr("src", iconURL);
+            day.append(icon);
+            $("#forecast").append(day);
+            
+            //Temperature
+            var temp = $("<p>");
+            temp.text(response.list[i].main.temp);
+            day.append(temp);
+            $("#forecast").append(day);
+
+            //Humidity
+            var humidity = $("<p>");
+            humidity.text(response.list[i].main.humidity);
+            day.append(humidity);
+            $("#forecast").append(day);
+
         }
     })
 }
 
-function weatherResult(longForecast) {
-    console.log(longForecast)
-    var results = longForecast.data;
-
-    //I need 5 different sets of data to return in 5 different spots, collectively, on my page
-
-    //display the first day
-    for (var i = 0; i < results.length; i++) {
-        //create main div//
-        var day1 = $("<div>");
-        //date//
-        var date1 = $("<p>");
-        date1.text(list[i].dt_text);
-        day1.append(date1);
-        //icon rep of weather conditions//
-        var icon1 = $("<img>");
-        icon1.text(list[i].weather.icon);
-        day1.append(icon1);
-        //temperature//
-        var temp1 = $("<p>");
-        temp1.text(list[i].main.temp);
-        day1.append(temp1);
-        //humidity//
-        var humidity1 = $("<p>");
-        humidity1.text(list[i].main.humidity);
-
-        $("#forecast").append(day1);
-    }
-}
 
 $("#run-search").on("click", function(event) {
     event.preventDefault();
     /*clear();*/
-    var longURL = buildLongURL();
+    buildLongURL();
     
     console.log("It searched for 5 day forecast!");
 });
