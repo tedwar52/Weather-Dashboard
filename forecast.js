cdf8238599d53a40470ffeaafd75fe180f556428
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-12-06 19:04:35 -0500

    created functions to save searched cities as buttons to be accessed later

--- forecast.js
+++ forecast.js
@@ -2,6 +2,8 @@
 *@returns {string}
 */
 
+const searchedCities = ["Charlotte", "Wilmington"]
+
 function buildQueryURL() {
 
     /*base url*/
@@ -20,7 +22,7 @@ function buildQueryURL() {
     
     const citySearch = $("#search-term").val().trim();
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apikey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + apikey;
 
 
     console.log(queryURL); //this is working
@@ -29,7 +31,9 @@ function buildQueryURL() {
  * @param {object} currentForecast
  */
 function forecastResult(currentForecast) {
-    console.log(currentForecast); //this is NOT
+    console.log(currentForecast); //this is NOT working
+
+    $("#daily").empty();
     var results = currentForecast.data;
     /*i will need to create a unique piece for each component i want to display from the results!.. then append & prepend each one*/
     for (var i = 0; i < results.length; i++) {
@@ -44,48 +48,66 @@ function forecastResult(currentForecast) {
         var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
         /*do i need to set the date as an attribute onto a new <p>?*/
         locationDiv.append(date);
-        $("#daily").append(locationDiv)
+        $("#daily").prepend(locationDiv)
         //weather description -- will need icon representation//
         var descriptionDiv = $("<div>");
         var description = $("<img>");
         description.text(weather[i].icon);
         descriptionDiv.append(description);
-        $("#daily").append(descriptionDiv);
+        $("#daily").prepend(descriptionDiv);
         //temperature//
         var tempDiv = $("<div>");
         var temp = $("<p>");
         temp.text(main[i].temp);
         tempDiv.append(temp);
-        $("#daily").append(tempDiv);
+        $("#daily").prepend(tempDiv);
         //humidity//
         var humidityDiv = $("<div>");
         var humidity = $("<p>");
         humidity.text(main[i].humidity);
         humidityDiv.append(humidity);
-        $("#daily").append(humidityDiv);
+        $("#daily").prepend(humidityDiv);
         //wind speed//
         var windDiv = $("<div>");
         var windSpeed = $("<p>");
         windSpeed.text(wind[i].speed);
         windDiv.append(windSpeed);
-        $("#daily").append(windDiv);
+        $("#daily").prepend(windDiv);
         //uv index//
         var uvDiv = $("<div>");
         var uvIndex = $("<p>");
         uvIndex.text(main[i].humidity);
         humidityDiv.append(humidity);
-        $("#daily").append(humidityDiv);
+        $("#daily").prepend(humidityDiv);
     };
+}
+
+function saveSearch() {
+
+    $("#old-cities").empty();
+
+    for (var i = 0; i < searchedCities.length; i++) {
+        var a = $("<button>");
+        a.addClass("city-btn");
+        a.attr("data-name", searchedCities[i]);
+        a.text(searchedCities[i]);
+        $("#old-cities").append(a);
+    }
 }
 
 $("#run-search").on("click", function(event) {
     event.preventDefault();
     /*clear();*/
+    var search = $("#search-term").val().trim();
     var queryURL = buildQueryURL();
     $.ajax({
         url: queryURL,
     }).then(forecastResult);
     console.log("It searched!");
+
+    searchedCities.push(search);
+    saveSearch();
+    
 });
 
 //----------------NOTES-----------------------------
